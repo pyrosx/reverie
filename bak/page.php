ebdaf7f8b8044f3021604844566c147acd4d3b16
pyrosx <pyrosx@gmail.com>
2013-07-10 11:50:19 +1000

    Colour tweaks, News uses posts

--- bak/page.php
+++ bak/page.php
@@ -7,19 +7,15 @@
 	<?php while (have_posts()) : the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
-				<h3 class="entry-title"><?php the_title(); ?></h3>
-				<?php echo '<time class="updated" datetime="'. get_the_time('c') .'" pubdate>'. sprintf(__('Posted on %s at %s.', 'reverie'), get_the_time('l, F jS, Y'), get_the_time()) .'</time>'; ?>
+				<h1 class="entry-title"><?php the_title(); ?></h1>
 			</header>
 			<div class="entry-content">
 				<?php the_content(); ?>
 			</div>
-			
-			<p class="byline author">
-				<?php echo '<a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .'</a>';?>
-			</p>
+			<footer>
 				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'reverie'), 'after' => '</p></nav>' )); ?>
 				<p><?php the_tags(); ?></p>
-			
+			</footer>
 		</article>
 	<?php endwhile; // End the loop ?>
 
--- page.php
+++ page.php
@@ -7,7 +7,7 @@
 	<?php while (have_posts()) : the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
+				<h3 class="entry-title"><?php the_title(); ?></h3>
 			</header>
 			<div class="entry-content">
 				<?php the_content(); ?>
