6f25f17741fb40f9e73a3c0418d16145aae4b216
pyrosx <pyrosx@gmail.com>
2015-03-30 11:32:18 +1100

    Added 404 page. Includes obnoxious dump of  object for now to aid troubleshooting

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<!-- Row for main content area -->
+	<div class="small-12 large-12 columns" role="main" id="main">
+	
+	<p>OOPS! Something went wrong.</p>
+	
+	<?php var_dump($_SERVER); ?>
+
+	</div>
+		
+<?php get_footer(); ?>
